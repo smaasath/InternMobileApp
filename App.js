903df0a9755ca4f5c014576087b681d5bb6e7e26
Aasath <aasadh2000@gmail.com>
2023-11-07 11:26:17 +0530

    final change

--- App.js
+++ App.js
@@ -24,14 +24,15 @@ export default function App() {
     <Provider store={Store}>
       <NavigationContainer>
         <Stack.Navigator>
-        <Stack.Screen name="Success" component={Success} options={{ headerShown: false }}/>
+
           <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
           <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
           <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
           <Stack.Screen name="Card" component={Card} />
-          <Stack.Screen name="Settings" component={Settings} options={{ headerShown: false }}/>
-          <Stack.Screen name="Dashboard" component={Dashboard} options={{ headerShown: false }}/>
-          
+          <Stack.Screen name="Settings" component={Settings} options={{ headerShown: false }} />
+          <Stack.Screen name="Dashboard" component={Dashboard} options={{ headerShown: false }} />
+          <Stack.Screen name="Success" component={Success} options={{ headerShown: false }} />
+
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
